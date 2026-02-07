aca347af7b93a78b85533fe44cdf8a6eca574f0c
Karan Sharma <karansharma2302578@gmail.com>
2026-02-07 23:12:58 +0530

    footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 import './Footer.css'
+import youtube_icon from '../../assets/youtube_icon.png'
+import twitter_icon from '../../assets/twitter_icon.png'
+import instagram_icon from '../../assets/instagram_icon.png'
+import facebook_icon from '../../assets/facebook_icon.png'
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className="footer">
+      <div className="footer-icons">
+        <img src={youtube_icon} alt="YouTube" />
+        <img src={twitter_icon} alt="Twitter" />
+        <img src={instagram_icon} alt="Instagram" />
+        <img src={facebook_icon} alt="Facebook" />
+      </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift cards</li>
+        <li>Media centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Cookie Preferences</li>
+        <li>Legal Notices</li>
+        <li>Corporate Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>© 2024 Netflix Clone. All rights reserved.</p>
+    </div>
   )
 }
 
